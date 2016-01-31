f18b550679faeacb1f5f29ae54218981a52f3117
Jamalah Bryan <jaibeee@gmail.com>
2016-01-31 00:19:24 +0000

    Added support for 3 UIText on each page. Added extra settings to configure new UIText. Added extra settings to move the pager left and right.

--- Auk/AukPage.swift
+++ Auk/AukPage.swift
@@ -2,107 +2,203 @@ import UIKit
 
 /// The view for an individual page of the scroll view containing an image.
 final class AukPage: UIView {
-  
-  // Image view for showing a placeholder image while remote image is being downloaded.
-  // The view is only created when a placeholder image is specified in settings.
-  weak var placeholderImageView: UIImageView?
-
-  // Image view for showing local and remote images
-  weak var imageView: UIImageView?
-  
-  // Contains a URL for the remote image, if any.
-  var remoteImage: AukRemoteImage?
-  
-  /**
-  
-  Shows an image.
-  
-  - parameter image: The image to be shown
-  - parameter settings: Auk settings.
-  
-  */
-  func show(image image: UIImage, settings: AukSettings) {
-    imageView = createAndLayoutImageView(settings)
-    imageView?.image = image
-  }
-  
-  /**
-  
-  Shows a remote image. The image download stars if/when the page becomes visible to the user.
-  
-  - parameter url: The URL to the image to be displayed.
-  - parameter settings: Auk settings.
-  
-  */
-  func show(url url: String, settings: AukSettings) {
-    if settings.placeholderImage != nil {
-      placeholderImageView = createAndLayoutImageView(settings)
+    
+    // Image view for showing a placeholder image while remote image is being downloaded.
+    // The view is only created when a placeholder image is specified in settings.
+    weak var placeholderImageView: UIImageView?
+    
+    // Image view for showing local and remote images
+    weak var imageView: UIImageView?
+    
+    // View for holding all text labels
+    weak var pageAuxView: UIView?
+    
+    // Label for showing promotion
+    weak var pagePromotion: UILabel?
+    
+    // Label for showing description
+    weak var pageDescption: UILabel?
+    
+    // Label for showing sub-description
+    weak var subPageDescption: UILabel?
+    
+    // Contains a URL for the remote image, if any.
+    var remoteImage: AukRemoteImage?
+    
+    /**
+     
+     Shows an image with description and sub-description.
+     
+     - parameter image: The image to be shown
+     - parameter settings: Auk settings.
+     
+     */
+    func show(image image: UIImage, description: String, subDescription: String, promotion: String, settings: AukSettings) {
+        imageView = createAndLayoutImageView(settings)
+        imageView?.image = image
+        
+        pageAuxView = createAndLayoutAuxView(settings.pageAuxView)
+        
+        pagePromotion = createAndLayoutLabel(settings.pagePromotion)
+        pagePromotion?.text = promotion
+        
+        pageDescption = createAndLayoutLabel(settings.pageDescription)
+        pageDescption?.text = description
+        
+        subPageDescption = createAndLayoutLabel(settings.pageSubDescription)
+        subPageDescption?.text = subDescription
     }
     
-    imageView = createAndLayoutImageView(settings)
-    
-    if let imageView = imageView {
-      remoteImage = AukRemoteImage()
-      remoteImage?.setup(url, imageView: imageView, placeholderImageView: placeholderImageView,
-        settings: settings)
+    /**
+     
+     Shows an image.
+     
+     - parameter image: The image to be shown
+     - parameter settings: Auk settings.
+     
+     */
+    func show(image image: UIImage, settings: AukSettings) {
+        imageView = createAndLayoutImageView(settings)
+        imageView?.image = image
     }
-  }
-  
-  /**
-
-  Called when the page is currently visible to user which triggers the image download. The function is called frequently each time scroll view's content offset is changed.
-  
-  */
-  func visibleNow(settings: AukSettings) {
-    remoteImage?.downloadImage(settings)
-  }
-  
-  /**
-  
-  Called when the page is currently not visible to user which cancels the image download. The method called frequently each time scroll view's content offset is changed and the page is out of sight.
-  
-  */
-  func outOfSightNow() {
-    remoteImage?.cancelDownload()
-  }
-  
-  /**
-  
-  Create and layout the remote image view.
-  
-  - parameter settings: Auk settings.
-  
-  */
-  func createAndLayoutImageView(settings: AukSettings) -> UIImageView {
-    let newImageView = AukPage.createImageView(settings)
-    addSubview(newImageView)
-    AukPage.layoutImageView(newImageView, superview: self)
-    return newImageView
-  }
-  
-  private static func createImageView(settings: AukSettings) -> UIImageView {
-    let newImageView = UIImageView()
-    newImageView.contentMode = settings.contentMode
-    return newImageView
-  }
-  
-  /**
-  
-  Creates Auto Layout constrains for the image view.
-  
-  - parameter imageView: Image view that is used to create Auto Layout constraints.
-  
-  */
-  private static func layoutImageView(imageView: UIImageView, superview: UIView) {
-    imageView.translatesAutoresizingMaskIntoConstraints = false
     
-    iiAutolayoutConstraints.fillParent(imageView, parentView: superview, margin: 0, vertically: false)
-    iiAutolayoutConstraints.fillParent(imageView, parentView: superview, margin: 0, vertically: true)
-  }
-  
-  func makeAccessible(accessibilityLabel: String?) {
-    isAccessibilityElement = true
-    accessibilityTraits = UIAccessibilityTraitImage
-    self.accessibilityLabel = accessibilityLabel
-  }
+    /**
+     
+     Shows a remote image. The image download stars if/when the page becomes visible to the user.
+     
+     - parameter url: The URL to the image to be displayed.
+     - parameter settings: Auk settings.
+     
+     */
+    func show(url url: String, settings: AukSettings) {
+        if settings.placeholderImage != nil {
+            placeholderImageView = createAndLayoutImageView(settings)
+        }
+        
+        imageView = createAndLayoutImageView(settings)
+        
+        if let imageView = imageView {
+            remoteImage = AukRemoteImage()
+            remoteImage?.setup(url, imageView: imageView, placeholderImageView: placeholderImageView,
+                settings: settings)
+        }
+    }
+    
+    /**
+     
+     Called when the page is currently visible to user which triggers the image download. The function is called frequently each time scroll view's content offset is changed.
+     
+     */
+    func visibleNow(settings: AukSettings) {
+        remoteImage?.downloadImage(settings)
+    }
+    
+    /**
+     
+     Called when the page is currently not visible to user which cancels the image download. The method called frequently each time scroll view's content offset is changed and the page is out of sight.
+     
+     */
+    func outOfSightNow() {
+        remoteImage?.cancelDownload()
+    }
+    
+    /**
+     
+     Create and layout the remote image view.
+     
+     - parameter settings: Auk settings.
+     
+     */
+    func createAndLayoutAuxView(settings: PageAuxSettings) -> UIView {
+        let newView = AukPage.createView(settings)
+        addSubview(newView)
+        AukPage.layoutView(newView, superview: self)
+        return newView
+    }
+    
+    private static func createView(settings: PageAuxSettings) -> UIView {
+        let newView = UIView(frame: settings.frame)
+        newView.layer.cornerRadius = settings.cornerRadius
+        newView.contentMode = settings.contentMode
+        newView.layer.backgroundColor = settings.backgroundColor.CGColor
+        newView.userInteractionEnabled = false
+        newView.tag = settings.tag
+        return newView
+    }
+    
+    /**
+     
+     Creates Auto Layout constrains for the view.
+     
+     - parameter view: Image view that is used to create Auto Layout constraints.
+     
+     */
+    private static func layoutView(view: UIView, superview: UIView) {
+        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+    }
+    
+    /**
+     
+     Create and layout the remote image view.
+     
+     - parameter settings: Auk settings.
+     
+     */
+    func createAndLayoutImageView(settings: AukSettings) -> UIImageView {
+        let newImageView = AukPage.createImageView(settings)
+        addSubview(newImageView)
+        AukPage.layoutImageView(newImageView, superview: self)
+        return newImageView
+    }
+    
+    private static func createImageView(settings: AukSettings) -> UIImageView {
+        let newImageView = UIImageView()
+        newImageView.contentMode = settings.contentMode
+        return newImageView
+    }
+    
+    /**
+     
+     Create and layout the labels.
+     
+     - parameter settings: AukBaseSettings.
+     
+     */
+    func createAndLayoutLabel(settings: AukBaseSettings) -> UILabel {
+        let newLabel = AukPage.createLabel(settings)
+        addSubview(newLabel)
+        return newLabel
+    }
+    
+    private static func createLabel(settings: AukBaseSettings) -> UILabel {
+        let newLabel = UILabel(frame: settings.frame)
+        newLabel.layer.cornerRadius = settings.cornerRadius
+        newLabel.contentMode = settings.contentMode
+        newLabel.textColor = settings.textColor
+        newLabel.font = settings.font
+        newLabel.layer.backgroundColor = settings.backgroundColor.CGColor
+        newLabel.textAlignment = settings.textAlignment
+        newLabel.tag = settings.tag
+        return newLabel
+    }
+    
+    /**
+     
+     Creates Auto Layout constrains for the image view.
+     
+     - parameter imageView: Image view that is used to create Auto Layout constraints.
+     
+     */
+    private static func layoutImageView(imageView: UIImageView, superview: UIView) {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        iiAutolayoutConstraints.fillParent(imageView, parentView: superview, margin: 0, vertically: false)
+        iiAutolayoutConstraints.fillParent(imageView, parentView: superview, margin: 0, vertically: true)
+    }
+    
+    func makeAccessible(accessibilityLabel: String?) {
+        isAccessibilityElement = true
+        accessibilityTraits = UIAccessibilityTraitImage
+        self.accessibilityLabel = accessibilityLabel
+    }
 }
--- Auk/AukSettings.swift
+++ Auk/AukSettings.swift
@@ -1,58 +1,212 @@
 import UIKit
 
 /**
-
-Appearance and behavior of the scroll view.
-
-*/
+ 
+ Appearance and behavior of the scroll view.
+ 
+ */
 public struct AukSettings {
-  
-  /// Determines the stretching and scaling of the image when its proportion are not the same as its  container.
-  public var contentMode = UIViewContentMode.ScaleAspectFit
-  
-  /// Image to be displayed when remote image download fails.
-  public var errorImage: UIImage?
-  
-  /// Settings for styling the scroll view page indicator.
-  public var pageControl = PageControlSettings()
-  
-  /// Enable paging for the scroll view. When true the view automatically scrolls to show the whole image.
-  public var pagingEnabled = true
-  
-  /// Image to be displayed while the remote image is being downloaded.
-  public var placeholderImage: UIImage?
-  
-  /// The duration of the animation that is used to show the remote images.
-  public var remoteImageAnimationIntervalSeconds: Double = 0.5
-  
-  /// Show horizontal scroll indicator.
-  public var showsHorizontalScrollIndicator = false
+    
+    /// Determines the stretching and scaling of the image when its proportion are not the same as its  container.
+    public var contentMode = UIViewContentMode.ScaleAspectFit
+    
+    /// Image to be displayed when remote image download fails.
+    public var errorImage: UIImage?
+    
+    /// Settings for styling the scroll view page indicator.
+    public var pageControl = PageControlSettings()
+    
+    /// Settings for styling the page auxilary container.
+    public var pageAuxView = PageAuxSettings()
+    
+    /// Settings for styling the page promotion.
+    public var pagePromotion = PagePromotionSettings()
+    
+    /// Settings for styling the page description.
+    public var pageDescription = PageDescriptionSettings()
+    
+    /// Settings for styling the page sub-description.
+    public var pageSubDescription = PageSubDescriptionSettings()
+    
+    /// Enable paging for the scroll view. When true the view automatically scrolls to show the whole image.
+    public var pagingEnabled = true
+    
+    /// Image to be displayed while the remote image is being downloaded.
+    public var placeholderImage: UIImage?
+    
+    /// The duration of the animation that is used to show the remote images.
+    public var remoteImageAnimationIntervalSeconds: Double = 0.5
+    
+    /// Show horizontal scroll indicator.
+    public var showsHorizontalScrollIndicator = false
 }
 
 /**
-
-Settings for page indicator.
-
-*/
+ 
+ Settings for page indicator.
+ 
+ */
 public struct PageControlSettings {
-  /// Background color of the page control container view.
-  public var backgroundColor = UIColor(red: 128/256, green: 128/256, blue: 128/256, alpha: 0.4)
-  
-  /// Corner radius of page control container view.
-  public var cornerRadius: Double = 13
-  
-  /// Color of the dot representing for the current page.
-  public var currentPageIndicatorTintColor: UIColor? = nil
-  
-  /// Padding between page indicator and its container
-  public var innerPadding = CGSize(width: 10, height: -5)
-  
-  /// Distance between the bottom of the page control view and the bottom of the scroll view.
-  public var marginToScrollViewBottom: Double = 8
-  
-  /// Color of the page indicator dot.
-  public var pageIndicatorTintColor: UIColor? = nil
-  
-  /// When true the page control is visible on screen.
-  public var visible = true
+    /// Frame of the page control container.
+    public var frame = CGRectMake(0, 0, 0, 0)
+    
+    /// Background color of the page control container view.
+    public var backgroundColor = UIColor(red: 128/256, green: 128/256, blue: 128/256, alpha: 0.4)
+    
+    /// Corner radius of page control container view.
+    public var cornerRadius: Double = 13
+    
+    /// Color of the dot representing for the current page.
+    public var currentPageIndicatorTintColor: UIColor? = nil
+    
+    /// Padding between page indicator and its container
+    public var innerPadding = CGSize(width: 10, height: -5)
+    
+    /// Distance between the bottom of the page control view and the bottom of the scroll view.
+    public var marginToScrollViewBottom: Double = 8
+    
+    /// Vertical position of pager.
+    public var verticalPosition: NSLayoutAttribute = NSLayoutAttribute.Bottom
+    
+    /// Horizontal position of pager.
+    public var horizontalPosition: NSLayoutAttribute = NSLayoutAttribute.Right
+    
+    /// Color of the page indicator dot.
+    public var pageIndicatorTintColor: UIColor? = nil
+    
+    /// When true the page control is visible on screen.
+    public var visible = true
 }
+
+/**
+ 
+ Settings for page auxillary container.
+ 
+ */
+public struct PageAuxSettings {
+    
+    /// Frame of the page auxillary container.
+    public var frame = CGRectMake(0, 0, 100, 50)
+    
+    /// Corner radius of page auxillary container.
+    public var cornerRadius: CGFloat = 0
+    
+    /// Content mode of the page auxillary container.
+    public var contentMode = UIViewContentMode.Bottom
+    
+    /// Background color of the page auxillary container.
+    public var backgroundColor = UIColor.whiteColor()
+    
+    /// Tag of the page auxillary container.
+    public var tag = 5;
+}
+
+/**
+ 
+ Settings for page promotion.
+ 
+ */
+public struct PagePromotionSettings: AukBaseSettings {
+    
+    /// Frame of the page promotion.
+    public var frame = CGRectMake(10, 15, 90, 15)
+    
+    /// Corner radius of page promotion.
+    public var cornerRadius: CGFloat = 5
+    
+    /// Content mode of the page promotion.
+    public var contentMode = UIViewContentMode.BottomLeft
+    
+    /// Text color of the page promotion text.
+    public var textColor = UIColor.whiteColor()
+    
+    /// Font of the page promotion text.
+    public var font = UIFont(name: "Arial", size: 12)
+    
+    /// Background color of the page promotion.
+    public var backgroundColor = UIColor(red: 255/256, green: 90/256, blue: 0/256, alpha: 1)
+    
+    /// Text alignment of the page promotion.
+    public var textAlignment = NSTextAlignment.Center
+    
+    /// Tag of the page promotion.
+    public var tag = 6;
+}
+
+/**
+ 
+ Settings for page description.
+ 
+ */
+public struct PageDescriptionSettings: AukBaseSettings {
+    
+    /// Frame of the page description.
+    public var frame = CGRectMake(10, 100, 200, 50)
+    
+    /// Corner radius of page description.
+    public var cornerRadius: CGFloat = 0
+    
+    /// Content mode of the page description.
+    public var contentMode = UIViewContentMode.BottomLeft
+    
+    /// Text color of the page description text.
+    public var textColor = UIColor.whiteColor()
+    
+    /// Font of the page description text.
+    public var font = UIFont(name: "Arial", size: 20)
+    
+    /// Background color of the page description.
+    public var backgroundColor = UIColor.clearColor()
+    
+    /// Text alignment of the page description.
+    public var textAlignment = NSTextAlignment.Left
+    
+    /// Tag of the page description.
+    public var tag = 7;
+}
+
+/**
+ 
+ Settings for page sub-description.
+ 
+ */
+public struct PageSubDescriptionSettings: AukBaseSettings {
+    
+    /// Frame of the page sub-description.
+    public var frame = CGRectMake(10, 125, 200, 50)
+    
+    /// Corner radius of page page sub-description.
+    public var cornerRadius: CGFloat = 0
+    
+    /// Content mode of the page sub-description.
+    public var contentMode = UIViewContentMode.BottomLeft
+    
+    /// Text color of the page sub-description text.
+    public var textColor = UIColor(red: 169/256, green: 169/256, blue: 169/256, alpha: 1)
+    
+    /// Font of the page sub-description text.
+    public var font = UIFont(name: "Arial", size: 15)
+    
+    /// Background color of the page sub-description.
+    public var backgroundColor = UIColor.clearColor()
+    
+    /// Text alignment of the page sub-description.
+    public var textAlignment = NSTextAlignment.Left
+    
+    /// Tag of the page sub-description.
+    public var tag = 8;
+}
+
+protocol AukBaseSettings {
+    
+    var frame: CGRect { get }
+    var cornerRadius: CGFloat { get }
+    var contentMode: UIViewContentMode { get }
+    var textColor: UIColor { get }
+    var font: UIFont? { get }
+    var backgroundColor: UIColor { get }
+    var textAlignment: NSTextAlignment { get }
+    var tag: Int { get }
+    
+}
+
